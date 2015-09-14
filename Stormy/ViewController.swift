ae5f8e44abbfd8a6f942751bb48234cbab9be358
Lydia Khashina <adatheleader@gmail.com>
2015-09-14 18:40:42 +0300

    refresh and light status bar added

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentPrecipitationLabel: UILabel?
     @IBOutlet weak var currentWeatherIcon: UIImageView?
     @IBOutlet weak var currentWeatherSummary: UILabel?
+    @IBOutlet weak var refreshButton: UIButton?
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView?
     
     private let forecastAPIKey = "176cd129c5d339df83810066b26b5d7d"
     let coordinat: (lat: Double, long: Double) = (37.8267,-122.423)
@@ -22,7 +24,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        retrieveWeatherForecast()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func retrieveWeatherForecast() {
         let forecastService = ForecastService(APIKey: forecastAPIKey)
         forecastService.getForecast(coordinat.lat, long: coordinat.long){
             (let currently) in
@@ -48,18 +58,35 @@ class ViewController: UIViewController {
                     if let summary = currentWeather.summary {
                         self.currentWeatherSummary?.text = summary
                     }
+                    
+                    self.toggleRefreshAnimation(false)
                 }
             }
         }
-        
-
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func refreshweather() {
+        toggleRefreshAnimation(true)
+        retrieveWeatherForecast()
     }
-
-
+    
+    func toggleRefreshAnimation(on: Bool) {
+        refreshButton?.hidden = on
+        if on {
+            activityIndicator?.startAnimating()
+        } else {
+            activityIndicator?.stopAnimating()
+        }
+    }
+    
+    
 }
+
+
+
+
+
+
+
 
