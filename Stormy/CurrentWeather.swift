2313aa0982aab8062e065b1cdd16b6c46c1fbecb
Lydia Khashina <adatheleader@gmail.com>
2015-09-14 17:14:34 +0300

    streamlining the icon enum

--- Stormy/CurrentWeather.swift
+++ Stormy/CurrentWeather.swift
@@ -21,68 +21,69 @@ enum Icon: String {
     case Cloudy = "cloudy"
     case PartlyCloudyDay = "partly-cloudy-day"
     case PartlyCloudyNight = "partly-cloudy-night"
-}
-
-
-
-
-struct CurrentWeather {
     
-    let temperature: Int?
-    let humidity: Int?
-    let precipProbabitily: Int?
-    let summary: String?
-    var icon: UIImage? = UIImage(named: "default.png")
     
-    init(weatherDictionary: [String: AnyObject]) {
-        temperature = weatherDictionary["temperature"] as? Int
-        if let humidityFloat = weatherDictionary["humidity"] as? Double{
-           humidity = Int(humidityFloat * 100)
-        } else {
-            humidity = nil
-        }
-        if let precipFloat = weatherDictionary["precipProbability"] as? Double {
-            precipProbabitily = Int(precipFloat * 100)
-        } else {
-            precipProbabitily = nil
-        }
-        summary = weatherDictionary["summary"] as? String
-        
-        if let iconString = weatherDictionary["icon"] as? String {
-            icon = weatherImageFromIconString(iconString)
-        }
-    }
-    
-    func weatherImageFromIconString(iconString: String) -> UIImage? {
+    func toImage() -> UIImage? {
         var imageName: String
         
-        if let iconValue = Icon(rawValue: iconString) {
-            switch iconValue {
-            case .ClearDay:
-                imageName = "clear-day.png"
-            case .ClearNight:
-                imageName = "clear-night.png"
-            case .Rain:
-                imageName = "rain.png"
-            case .Snow:
-                imageName = "snow.png"
-            case .Sleet:
-                imageName = "sleet.png"
-            case .Wind:
-                imageName = "wind.png"
-            case .Fog:
-                imageName = "fog.png"
-            case .Cloudy:
-                imageName = "cloudy.png"
-            case .PartlyCloudyDay:
-                imageName = "cloudy-day.png"
-            case .PartlyCloudyNight:
-                imageName = "cloudy-night.png"
-            }
-        } else {
-            imageName = "default.png"
+        
+        switch self {
+        case .ClearDay:
+            imageName = "clear-day.png"
+        case .ClearNight:
+            imageName = "clear-night.png"
+        case .Rain:
+            imageName = "rain.png"
+        case .Snow:
+            imageName = "snow.png"
+        case .Sleet:
+            imageName = "sleet.png"
+        case .Wind:
+            imageName = "wind.png"
+        case .Fog:
+            imageName = "fog.png"
+        case .Cloudy:
+            imageName = "cloudy.png"
+        case .PartlyCloudyDay:
+            imageName = "cloudy-day.png"
+        case .PartlyCloudyNight:
+            imageName = "cloudy-night.png"
+      
         }
         return UIImage(named: imageName)
     }
-    
 }
+
+
+    struct CurrentWeather {
+        
+        let temperature: Int?
+        let humidity: Int?
+        let precipProbabitily: Int?
+        let summary: String?
+        var icon: UIImage? = UIImage(named: "default.png")
+        
+        init(weatherDictionary: [String: AnyObject]) {
+            temperature = weatherDictionary["temperature"] as? Int
+            if let humidityFloat = weatherDictionary["humidity"] as? Double {
+                humidity = Int(humidityFloat * 100)
+            } else {
+                humidity = nil
+            }
+            if let precipFloat = weatherDictionary["precipProbability"] as? Double {
+                precipProbabitily = Int(precipFloat * 100)
+            } else {
+                precipProbabitily = nil
+            }
+            summary = weatherDictionary["summary"] as? String
+            
+            if let iconString = weatherDictionary["icon"] as? String,
+                let weatherIcon: Icon = Icon(rawValue: iconString) {
+                    icon = weatherIcon.toImage()
+            }
+        }
+    }
+
+
+
+
