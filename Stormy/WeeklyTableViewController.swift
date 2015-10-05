d1884233f5880b70402a5ea2b2282668946c3028
Lydia Khashina <adatheleader@gmail.com>
2015-10-05 16:39:30 +0300

    edited highligh and separator of tabelView

--- Stormy/WeeklyTableViewController.swift
+++ Stormy/WeeklyTableViewController.swift
@@ -58,6 +58,10 @@ class WeeklyTableViewController: UITableViewController {
         // return the number of sections
         return 1
     }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Forecast"
+    }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // return the number of rows
@@ -75,6 +79,25 @@ class WeeklyTableViewController: UITableViewController {
         cell.dayLabel.text = dailyWeather.day
         
         return cell
+    }
+    
+    // MARK: - Delagate Methods
+    
+    override func tableView(tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        view.tintColor = UIColor(red: 170/255.0, green: 131/255.0, blue: 224/255.0, alpha: 1.0)
+        
+        if let header = view as? UITableViewHeaderFooterView {
+            header.textLabel?.font = UIFont(name: "HelveticaNeue-Thin", size: 14.0)
+            header.textLabel?.textColor = UIColor.whiteColor()
+        }
+    }
+    
+    override func tableView(tableView: UITableView, didHighlightRowAtIndexPath indexPath: NSIndexPath) {
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        cell?.contentView.backgroundColor = UIColor(red: 165/255.0, green: 142/255.0, blue: 203/255.0, alpha: 1.0)
+        let highlightView = UIView()
+        highlightView.backgroundColor = UIColor(red: 165/255.0, green: 142/255.0, blue: 203/255.0, alpha: 1.0)
+        cell?.selectedBackgroundView = highlightView
     }
     
     // MARK: - Weather Fetching
